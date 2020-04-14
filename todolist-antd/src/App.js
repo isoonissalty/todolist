71529b6e6d38609527d0bed9c4c73481af1b2662
isoonissalty <isoon.ph@gmail.com>
2020-04-15 04:34:18 +0700

    add todolist-antd

---
+++ todolist-antd/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Layout, Row, Col, Button, Input, List } from 'antd'
+import './app.css'
+
+const { Content } = Layout
+
+function App() {
+
+  const [text, setText] = useState('')
+  const [todos, setTodo] = useState([])
+
+  const addTodo = () => {
+    if (text !== '') {
+      setTodo([
+        text,
+        ...todos
+      ])
+    }
+
+    setText('')
+  }
+
+  return (
+    <Layout className="layout">
+      <Content style={{ padding: '50px 50px' }}>
+        <Row gutter={16} className="site-layout-content">
+          <Col span={21}>
+            <Input placeholder="type todo here..." value={text} onChange={(e) => setText(e.target.value)} />
+          </Col>
+          <Col span={3}>
+            <Button block type="primary" onClick={addTodo}>ADD</Button>
+          </Col>
+        </Row>
+        <Row gutter={16} className="site-layout-content" style={{paddingBottom: 24}}>
+          <Col span={24}>
+            <List
+              size="large"
+              bordered
+              dataSource={todos}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
+}
+
+export default App;
