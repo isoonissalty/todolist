fcd1fabab08fa233003f25359e54ebc4657107ad
isoonissalty <isoon.ph@gmail.com>
2020-05-12 11:40:28 +0700

    add color range filter and isdone filter

--- 05-todolist-advance/src/App.js
+++ 05-todolist-advance/src/App.js
@@ -5,7 +5,10 @@ const today = new Date()
 function App() {
 
   const [todos, setTodos] = useState([])
+  const [filter, setFilter] = useState('none')
+
   const [form, setForm] = useState({
+    id: todos.length,
     title: '',
     detail: '',
     due: formatDate(today),
@@ -25,13 +28,43 @@ function App() {
     e.preventDefault()
     setTodos([...todos, form])
     setForm({
+      id: todos.length+1,
       title: '',
       detail: '',
-      due: new Date(),
+      due: formatDate(today),
       color: '#000000',
       isDone: false,
     })
   }
+
+  const updateTodo = (id) => {
+    let temp = {...todos[id]}
+    temp = {
+      ...temp,
+      isDone: !todos[id].isDone
+    }
+    let newTodos = todos.map((val, index) => {
+      if (index === id) return temp
+      else return val
+    })
+
+    console.log(newTodos)
+
+    setTodos(newTodos)
+  }
+
+  const todoList = (value, index) => {
+    return (
+      < li key={index} style={{ color: value.color }}>
+        id: {value.id} <br />
+        title: {value.title} <br />
+        detail: {value.detail} <br />
+        due: {value.due} <br />
+        <button onClick={() => updateTodo(value.id)}>{value.isDone ? 'uncheck' : 'check'}</button>
+      </li>
+    )
+  }
+
 
   return (
     <div>
@@ -55,18 +88,30 @@ function App() {
         <input type='submit' value="add todo" />
       </form>
       <hr />
-      <ul>
+      <div>
+        <span>filter: </span>
+        <button onClick={() => setFilter('done')}>done</button>
+        <button onClick={() => setFilter('not done')}>not done</button>
         {
-          todos.map((value, index) => (
-          <li style={{color: value.color}}>
-            title: {value.title} <br />
-            detail: {value.detail} <br />
-            due: {value.due}
-          </li>
+          ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ff00ff', '#000000'].map((value, index) => (
+            <button key={index} style={{ backgroundColor: value, width: 40, height: 15, margin: 4 }} onClick={() => setFilter(value)}></button>
           ))
         }
+        <button onClick={() => setFilter('none')}>clear</button>
+      </div>
+      <ul>
+        {
+          (filter === 'none') ?
+            todos.map((value, index) => todoList(value, index)) :
+            (filter === 'done') ?
+              todos.filter(val => val.isDone).map((value, index) => todoList(value, index))
+              : (filter === 'not done') ?
+                todos.filter(val => !val.isDone).map((value, index) => todoList(value, index))
+                :
+                todos.filter(val => formatColor(val.color) === filter).map((value, index) => todoList(value, index))
+        }
       </ul>
-    </div>
+    </div >
   );
 }
 
@@ -76,6 +121,19 @@ const formatDate = (today) => {
   let d = today.getDate()
   let t = `${y}-${m < 10 ? `0${m}` : `${m}`}-${d < 10 ? `0${d}` : d}`
   return t
+}
+
+const formatColor = (filter) => {
+  let r = parseInt(`0x${filter.slice(1, 3)}`)
+  let g = parseInt(`0x${filter.slice(3, 5)}`)
+  let b = parseInt(`0x${filter.slice(5, 7)}`)
+  r = Math.round(r / Math.floor(255 / 3)) + 1
+  g = Math.round(g / Math.floor(255 / 3)) + 1
+  b = Math.round(b / Math.floor(255 / 3)) + 1
+  let colorRange = ['#000000', '#0000ff', '#00ff00', '#00ffff', '#ff0000', '#ff00ff', '#ffff00']
+  console.log(`${Math.ceil(1 - (1 / r))}${Math.ceil(1 - (1 / g))}${Math.ceil(1 - (1 / b))}`)
+  console.log(parseInt(`${Math.ceil(1 - (1 / r))}${Math.ceil(1 - (1 / g))}${Math.ceil(1 - (1 / b))}`, 2))
+  return colorRange[parseInt(`${Math.ceil(1 - (1 / r))}${Math.ceil(1 - (1 / g))}${Math.ceil(1 - (1 / b))}`, 2)]
 }
 
 export default App;
